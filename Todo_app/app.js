a22c3c6aac7c92e5e303aa97eb92e3192a2d4bda
Swaminath Nalwar <84504953+swaminath8358@users.noreply.github.com>
2023-01-04 13:26:16 +0530

    Add files via upload

---
+++ Todo_app/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require('mongoose')
+
+
+
+const app =  express()
+
+
+mongoose.connect("mongodb://localhost/todo_express", {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+});
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.use(require("./routes/index"));
+app.use(require("./routes/todo"));
+
+app.listen(3000, () => console.log("Server started : 3000"))
